5eda64ecb506a3b189f5af2f12579eb3c48c2f0c
cdrojasm <cdrojasm@unal.edu.co>
2024-03-18 21:21:48 -0500

    :rocket: change the entry point of start service file. fix some typo errors

--- api/start_services.sh
+++ api/start_services.sh
@@ -1,24 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 echo "starting HEAT API"
-if [ ! -f ./.env ];then
+if [ ! -f ./.env ]; then
 	echo "environment file not found"
 	exit 1
 fi
 source .env
-PORT=$API_PORT
-ENABLE_CONNECTIONS=$ENABLE_CONNECTIONS
+PORT=$DEPLOYMENT_API_PORT
 TIME_OUT=$TIME_OUT
 WORKERS=$WORKERS
-MODE=$MODE
+MODE=$LOG_LEVEL_DEBUG
 
-if [ $DEBUG_CONTAINER -eq "true"];then
+if [ $DEBUG_CONTAINER -eq "true" ]; then
 	# this mode is useful to debug problems during container start up
 	echo "starting container debug mode"
 	sleep 3600
 fi
 
+echo "starting gunicorn"
+echo "WORKERS = $WORKERS"
+echo "TIME_OUT = $TIME_OUT"
+echo "PORT = $PORT"
+echo "MODE = $MODE"
+
 gunicorn --bind "0.0.0.0:$PORT" \
-	-- worker-class gevent \
+	--worker-class gevent \
 	--workers "$WORKERS" \
 	--log-level $MODE \
 	--timeout "$TIME_OUT" patched:app
